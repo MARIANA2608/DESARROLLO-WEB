8cfc042c4dd179d1e7ead422e38adeb2dfbb5634
MARIANA2608 <ma.cascog@uea.edu.ec>
2026-02-28 14:34:56 -0500

    tarea semana 10 y 11

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -1,21 +1,89 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect, flash
+from form import ProductoForm
+from inventario.bd import init_db, get_db_connection
+from inventario.inventario import Inventario
+from inventario.productos import Producto
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mi_clave_secreta'
+init_db()
+inventario = Inventario()
+inventario.cargar_desde_db()
 
-# Ruta principal
 @app.route('/')
 def inicio():
-    return """
-    <h1>Sistema de Gestión de Inventario – TechStore</h1>
-    <p>Bienvenido al sistema de administración de productos.</p>
-    <ul>
-        <li>Consultar producto: /item/A123</li>
-        <li>Consultar categoría: /categoria/electronica</li>
-        <li>Ver stock por cantidad: /stock/10</li>
-    </ul>
-    """
+    return render_template("index.html")
 
-# Ruta dinámica para consultar producto por código
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'Bienvenido, {nombre}!'
+
+@app.route('/contactos')
+def contact():
+    return render_template("contactos.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# ruta de productos
+@app.route('/productos/nuevo', methods=['GET', 'POST'])
+def producto_nuevo():
+    form = ProductoForm()
+    if form.validate_on_submit():
+        nombre = form.nombre.data
+        descripcion = form.descripcion.data
+        cantidad = form.cantidad.data
+        precio = form.precio.data
+        inventario.agregar_producto(nombre, descripcion, cantidad, precio)
+        flash('Producto agregado exitosamente', 'success')
+        return redirect(url_for('productos_listar'))
+    return render_template('producto_form.html', form=form)
+
+# ruta para listar productos
+@app.route('/productos')
+def productos_listar():
+    inventario.cargar_desde_db()  # Asegurarse de cargar los productos más recientes
+    productos = list(inventario.productos.values())
+    return render_template('productos.html', productos=productos)
+
+# ruta para editar producto
+@app.route('/productos/editar/<int:id>', methods=['GET', 'POST'])
+def producto_editar(id):
+    producto = inventario.productos.get(id)
+    if not producto:
+        flash('Producto no encontrado', 'danger')
+        return redirect(url_for('productos_listar'))
+    
+    form = ProductoForm(obj=producto)
+    if form.validate_on_submit():
+        nombre = form.nombre.data
+        descripcion = form.descripcion.data
+        cantidad = form.cantidad.data
+        precio = form.precio.data
+        inventario.actualizar_producto(id, nombre, descripcion, cantidad, precio)
+        flash('Producto actualizado exitosamente', 'success')
+        return redirect(url_for('productos_listar'))
+    
+    return render_template('../productos/producto_editar.html', form=form, producto=producto)
+
+# ruta para eliminar producto
+@app.route('/productos/eliminar/<int:id>', methods=['POST'])
+def producto_eliminar(id):
+    inventario.eliminar_producto(id)
+    flash('Producto eliminado exitosamente', 'success')
+    return redirect(url_for('productos_listar'))
+
+
+
+    
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 @app.route('/item/<codigo>')
 def item(codigo):
     return f"""
---
+++ Proyecto/inventario/productos.py
@@ -0,0 +1,23 @@
+# clase producto 
+class Producto:
+    def __init__(self, id, nombre, descripcion, cantidad, precio):
+        self.id = id
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.cantidad = cantidad
+        self.precio = precio
+
+    # tuple 
+    def to_tuple(self):
+        return (self.id, self.nombre, self.descripcion, self.cantidad, self.precio)
+    
+    # dict 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'nombre': self.nombre,
+            'descripcion': self.descripcion,
+            'cantidad': self.cantidad,
+            'precio': self.precio
+        }
+    
